a6b1d9e1741bc2946775edd1c826d71b0a3c15c1
Croaker <249576@student.pwr.edu.pl>
2023-03-12 18:21:00 +0100

    message

--- src/deimos_server.py
+++ src/deimos_server.py
@@ -2,21 +2,25 @@
 import socket
 
 host = socket.gethostbyname(socket.gethostname())  #24:50 dla VB
-port = 2137
+port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #utworzenie obiektu socket z użyciem konstruktora socket (do użycia z internetem AF_INET, z protokołem TCP - sock_stream)
 server.bind((host, port))
-
-server.listen(10)
+server.listen()
+communication_socket, address = server.accept()  
 
 def deimosConnect():
-    while True:
-        communication_socket, address = server.accept()  
+    done = False
+    while not done:
         #server.accept zwraca adres nadchodzącego połączenia i inny socket którego możemy użyć do połączenia - 
         #dla każdego połączenia tworzymy nowy socket do komunikacji z klientem server socket jest tylko do akceptowania przychodzących połączeń
-        print(f"Connected with {address}" )
-        message = communication_socket.recv(4096).decode() #recv(rozmiar bufora w bitach)  wiadomość jest otrzymana w formacie bitowym -> musi zostać zdekodowana
-        print(f"Message is: {message}")
-        communication_socket.send("Gotcha... Anything else ?".encode('utf-8')) #utf-8 -> 8-bit Unicode Transformation Format
-        communication_socket.close()
-        print(f"Connection with {address} ended")
+        incoming = communication_socket.recv(4096).decode('utf-8')
+        if incoming == " ":
+            communication_socket.close()
+            done = True
+        else:
+            print(incoming)
+        #communication_socket.close()
+        communication_socket.send(input("Message: ").encode('utf-8'))
+
+deimosConnect()
