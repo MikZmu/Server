2a1f76a315d86ac846f01f32309cd1548b6050cd
MikZmu <249576@student.pwr.edu.pl>
2023-04-26 22:38:30 +0200

    adrgphijkmn'

--- src/deimos_server.py
+++ src/deimos_server.py
@@ -12,6 +12,7 @@ import pyaudio
 import wave
 import pickle
 import base64
+import concurrent.futures
 #import requests 
 
 
@@ -19,12 +20,19 @@ import base64
 
 
 
-global host, port, server, linuxMode
+global host, port, server, linuxMode, buffer
 host = '0.0.0.0'
 port = 9999
 global state
 state = ['isLinux', 'streaming', 'connected', 'connecting', 'binding', 'quitPrompt']
 global curState
+buffer = 65536
+q = queue.Queue(maxsize=30)
+
+
+
+
+
 
 
 #def getip():
@@ -32,7 +40,11 @@ global curState
     #print('My public IP address is: {}'.format(ip))
 
 
+
+
+
 def kill_process_using_port(port):
+    print("kill process")
     try:
         pid = subprocess.run(
             ['lsof', '-t', f'-i:{port}'], text=True, capture_output=True
@@ -49,6 +61,7 @@ def bind():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #utworzenie obiektu socket z użyciem konstruktora socket (do użycia z internetem AF_INET, z protokołem TCP - sock_stream)
     global curState
     curState = 'binding'
+    print('binding')
     if(linuxMode == 1):
         kill_process_using_port(9999) 
     try:
@@ -62,6 +75,7 @@ def bind():
         bind()
 
 def conn():
+    print('connecting')
     global curState
     curState = 'connecting'
     try:
@@ -103,6 +117,12 @@ def command():
                     handle(msg)
             except:
                 time.sleep(1)
+
+
+
+
+
+
 
 
 def receive():
@@ -155,144 +175,120 @@ def handle(handled):
         elif(curState == 'connected'):
             phobos.send(str(handled).encode('ascii'))
         if handled == 'film':
-            #video_stream()
+            videoTransmission(r"C:\Users\52 Blue\Documents\wbudowane\deimos\src\pepe.mp4")
 
 
+def videoTransmission(video):
+    command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format(video,'temp.wav')
+    os.system(command)
 
-"""def deimosFile():
-    fileName  = input('provide file name plis: ')
-    file = open(fileName , 'rb') #rb - reading bytes mode // otwiera plik w trybie czytania bitów
-    fileSize = os.path.getsize(fileName)
-    extension = fileName.split('.')[1]
-    communication_socket.send(fileName.encode())
-    communication_socket.send(str(fileSize).encode())
-    data = file.read() # zwraca plik
-    communication_socket.sendall(data)
-    communication_socket.send(b'<END>')#wysyła tag końcowy, tak aby druga maszyna wiedziała jaki jest koniec pliku w bitach :_)
-    file.close()
-    communication_socket.close()
-
-
-def remoteControl():
-    while True:
-        communication_socket, address = server.accept()
-        menuThread = threading.Thread(target = menu) 
-        menuThread.start
-        sendThread = threading.Thread(target = sending)
-        recvThread = threading.Thread(target = recv)
-
-def recv():
-    while True:
+    """def vidBind():
+        global server_socket
+        server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        host_name = socket.gethostname()
+        host_ip = '0.0.0.0'
+        vidPort = 9998
         try:
-            chooser = communication_socket.recv('1024')
-            
-            menu(incoming)
+            server_socket.bind((host_ip, vidPort))
+            server_socket.listen()
+            vidConn()
+        except Exception as e:
+            print(str(e))
+            #kill_process_using_port(11111)
+            time.sleep(5)
+            vidBind()
+
+    def vidConn():
+        try:
+            global phobosVid, addressVid
+            phobosVid, addressVid= server_socket.accept()
+            print(f'Connected with {addressVid}')
         except:
-            sleep()
-
-def menu(chooser):
-    if chooser == "quit":
-        communication_socket.close()
-        done = True
-    else:
-        if(chooser == 1):
-            deimosChat()
-        if(chooser == 2):
-            deimosFile()
-        if(chooser == 3):
-            deimosVid()
+            print('Awaiting connection... ')
+            time.sleep(5)
+            vidConn()"""
 
 
 
 
+    def vidInfo():
+        global vid
+        vid = cv2.VideoCapture(video)
+        global FPS
+        FPS = vid.get(cv2.CAP_PROP_FPS)
+        global TS
+        TS = (0.5/FPS)
+        global BREAK
+        BREAK=False
+        print('FPS:',FPS,TS)
+        totalNoFrames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+        durationInSeconds = float(totalNoFrames) / float(FPS)
+        d=vid.get(cv2.CAP_PROP_POS_MSEC)
+        print(durationInSeconds,d)
 
 
 
-def menu2():
-    print('Type 1 to chat, 2 - To send .txt, 3 - To send video quit to quit' )
-    sending = input('Make your choice!: ')
-    incoming = communication_socket.recv(4096).decode('utf-8')
-    if sending == "quit":
-        communication_socket.close()
-        done = True
-    else:
-        communication_socket.send(sending.encode())
-        if(sending == 1):
-            deimosChat()
-        if(sending == 2):
-            deimosFile()
-        if(sending == 3):
-            deimosVid()
-
-    if incoming == "quit":
-        communication_socket.close()
-        done = True
-    else:
-        if(incoming == 1):
-            deimosChat()
-        if(incoming == 2):
-            deimosFile()
-        if(incoming == 3):
-            deimosVid()"""
-
-
-
-
-"""q = queue.Queue(maxsize=10)
-absolutePath = os.path.dirname(os.path.realpath(__file__))
-relativePath = r"videos\vid1.mp4"
-filename = os.path.join(absolutePath, relativePath)  
-command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format(filename,'temp.wav')
-os.system(command)
-
-BUFF_SIZE = 65536
-vid = cv2.VideoCapture(filename)
-FPS = vid.get(cv2.CAP_PROP_FPS)
-global TS
-TS = (0.5/FPS)
-BREAK=False
-totalNoFrames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
-durationInSeconds = float(totalNoFrames) / float(FPS)
-d=vid.get(cv2.CAP_PROP_POS_MSEC)
-
-
-
-def video_stream():
-    global TS
-    fps,st,frames_to_count,cnt = (0,0,1,0)
-    cv2.namedWindow('TRANSMITTING VIDEO')        
-    cv2.moveWindow('TRANSMITTING VIDEO', 10,30) 
-    while True:
-        msg,client_addr = phobos.recvfrom(BUFF_SIZE)
-        print('GOT connection from ',client_addr)
+    
+    def video_stream_gen():
         WIDTH=400
-        
-        while(True):
-            frame = q.get()
-            encoded,buffer = cv2.imencode('.jpeg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
-            message = base64.b64encode(buffer)
-            phobos.sendto(message,client_addr)
-            frame = cv2.putText(frame,'FPS: '+str(round(fps,1)),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-            if cnt == frames_to_count:
-                try:
-                    fps = (frames_to_count/(time.time()-st))
-                    st=time.time()
-                    cnt=0
-                    if fps>FPS:
-                        TS+=0.001
-                    elif fps<FPS:
-                        TS-=0.001
-                    else:
-                        pass
-                except:
-                    pass
-            cnt+=1
-            
-            
-            
-            cv2.imshow('TRANSMITTING VIDEO', frame)
-            key = cv2.waitKey(int(1000*TS)) & 0xFF	
-            if key == ord('q'):
+        while(vid.isOpened()):
+            try:
+                _,frame = vid.read()
+                frame = imutils.resize(frame,width=WIDTH)
+                q.put(frame)
+            except:
                 os._exit(1)
-                TS=False
-                break	"""
+        print('Player closed')
+        global BREAK
+        BREAK=True
+        vid.release()
+
+    def video_stream():
+        global TS
+        fps,st,frames_to_count,cnt = (0,0,1,0)
+        """cv2.namedWindow('TRANSMITTING VIDEO')        
+        cv2.moveWindow('TRANSMITTING VIDEO', 10,30) """
+        while True:
+            WIDTH=400
+            
+            while(True):
+                frame = q.get()
+                encoded,buffer = cv2.imencode('.jpeg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+                message = base64.b64encode(buffer)
+                phobos.sendto(message,address)
+                frame = cv2.putText(frame,'FPS: '+str(round(fps,1)),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+                if cnt == frames_to_count:
+                    try:
+                        fps = (frames_to_count/(time.time()-st))
+                        st=time.time()
+                        cnt=0
+                        if fps>FPS:
+                            TS+=0.001
+                        elif fps<FPS:
+                            TS-=0.001
+                        else:
+                            pass
+                    except:
+                        pass
+                cnt+=1
+                
+                
+                
+                cv2.imshow('TRANSMITTING VIDEO', frame)
+                key = cv2.waitKey(int(1000*TS)) & 0xFF	
+                if key == ord('q'):
+                    os._exit(1)
+                    TS=False
+                    break
+    vidInfo()
+    #vidBind()
+    gen_thread = threading.Thread(target=video_stream_gen)
+    stream_thread = threading.Thread(target=video_stream)
+    gen_thread.start()
+    stream_thread.start()
+    
+
+
+
+    
+
